68ecf1cb2a98569d863ad94eb7d427e5af77e36c
jwzhub-maker <jwzhub-maker@github.com>
2021-11-24 18:47:37 +0800

    Add Progress bar to the pages when loading the search results.

--- pages/_app.js
+++ pages/_app.js
@@ -3,20 +3,33 @@ import "../styles/global.css";
 import Router from "next/router";
 
 import ProgressBar from "@badrap/bar-of-progress";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
 
-const progress = new ProgressBar({
-  size: 4,
-  color: "#FE595E",
-  className: "z-50",
-  delay: 1000,
+// const progress = new ProgressBar({
+//   size: 4,
+//   color: "#FE595E",
+//   className: "z-0",
+//   delay: 1000,
+// });
+
+// Router.events.on("routerChangeStart", progress.start);
+// Router.events.on("routerChangeComplete", progress.finish);
+// Router.events.on("routerChangeError", progress.finish);
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: "ease",
+  speed: 800,
+  showSpinner: true,
 });
 
-Router.events.on("routerChangeStart", progress.start);
-Router.events.on("routerChangeComplete", progress.finish);
-Router.events.on("routerChangeError", progress.finish);
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/search.js
+++ pages/search.js
@@ -53,7 +53,7 @@ function Search({ searchResults, weatherData }) {
         </section>
 
         <section className="hidden xl:inline-flex xl:min-w-[600px]">
-          <Map searchResults={searchResults} />
+          <Map searchResults={searchResults}/>
         </section>
       </main>
 
